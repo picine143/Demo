25af165157e9250fc2c537860ff47b198f212985
Daaim <sheikhdaaim143@gmail.com>
2025-06-24 14:03:06 +0530

    final

--- demo1/src/components/minors/CatagoryCard.tsx
+++ demo1/src/components/minors/CatagoryCard.tsx
@@ -1,30 +1,24 @@
 import React from "react";
 import { Card } from "../ui/card";
 import { Button } from "../ui/button";
+import Image from "next/image";
 
 type props = {
   Heading: string;
   Description: string;
   btn_name: string;
-  btn_address: string;
   img_url: string;
 };
 
-const CatagoryCard = ({
-  Heading,
-  Description,
-  btn_name,
-  btn_address,
-  img_url,
-}: props) => {
+const CatagoryCard = ({ Heading, Description, btn_name, img_url }: props) => {
   return (
     <Card className=" w-90 mx-auto px-4 py-4 gap-0  box-border">
       <h2 className="font-extrabold text-2xl  ">{Heading}</h2>
       <p className="font-light text-xs ">{Description}</p>
-      <img
+      <Image
         src={img_url}
         className="w-full h-75 object-cover mx-auto rounded-xl my-4"
-        alt=""
+        alt={Heading}
       />
       <Button variant={"outline"} className="bg-black text-white ">
         {btn_name}
