0ec5de4905bed8f2b566222b98dab6ae13fb2317
Daaim <sheikhdaaim143@gmail.com>
2025-06-24 14:05:38 +0530

    finally final

--- demo1/src/app/page.tsx
+++ demo1/src/app/page.tsx
@@ -5,28 +5,24 @@ import CatagoryCard from "@/components/minors/CatagoryCard";
 export default function Home() {
   return (
     <div className="w-full justify-center ">
-      
       <Hero />
       <section className="flex flex-col md:flex-row gap-8 ">
         <CatagoryCard
           Heading="Singers"
           Description="This is a test to check the text wrap capacity of the card item it does work but how long will it work."
           btn_name="Sing"
-          btn_address="/artists"
           img_url="/singer.png"
         />
         <CatagoryCard
           Heading="Dancers"
           Description="This is a test to check the text wrap capacity of the card item it does work but how long will it work."
           btn_name="Dance"
-          btn_address="/artists"
           img_url="/dancer.png"
         />
         <CatagoryCard
           Heading="DJ's"
           Description="This is a test to check the text wrap capacity of the card item it does work but how long will it work."
           btn_name="Jazz"
-          btn_address="/artists"
           img_url="/dancer.png"
         />
       </section>
